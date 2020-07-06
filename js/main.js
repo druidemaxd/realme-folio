383d8c6ad56d0777266a938a644c7dd0ae9780a6
druidemaxd <druidemaxd@gmail.com>
2020-07-06 04:03:21 +0100

    Mon portfolio:redesign, fonctionnalitées js, mobile-first

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/*===== menu show=====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*===== Active and Remove menu=====*/
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkaction() {
+  //active link
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  // Remove menu mobile
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkaction));
+
+/*===== Scroll reveal animation=====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+// Scroll Home
+sr.reveal(".home_title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home_img", { delay: 400 });
+sr.reveal(".home_social-icon", { interval: 200 });
+
+// Scroll About
+sr.reveal(".about_img", {});
+sr.reveal(".about_subtitle", { delay: 200 });
+sr.reveal(".about_text", { delay: 400 });
+
+// Scroll skills
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_text", { delay: 200 });
+sr.reveal(".skills_data", { interval: 200 });
+sr.reveal(".skills_img", { delay: 400 });
+
+// Scroll Works
+sr.reveal(".work_img", { interval: 200 });
+
+// Scroll Contact
+sr.reveal(".contact_input", { interval: 200 });
